8b85b3a14040c8f545277b9aa6e059d22a4a766f
Deepak Chakravarti <deepakcchakravarti@gmail.com>
2025-12-08 22:04:49 +0530

    add logout button in navbar and some changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useStoreContext } from "../contextApi/ContextApi";
 
 export const NavBar = () => {
+  const {token,setToken} = useStoreContext();
+  const logOutHandler = ()=>{
+    localStorage.removeItem("JWT_TOKEN");
+    setToken(null);
+    navigate("/login");
+  }
   return (
     <header className="w-full h-16 bg-black text-white flex items-center">
       <div className="max-w-7xl mx-auto px-6 flex justify-between items-center w-full">
@@ -15,7 +22,18 @@ export const NavBar = () => {
           <Link to="/features" className="hover:text-gray-300">Features</Link>
           <Link to="/pricing" className="hover:text-gray-300">Pricing</Link>
           <Link to="/about" className="hover:text-gray-300">About</Link>
-          <Link to="/register" className="hover:text-gray-300">SignUp</Link>
+           {token && (
+          <Link to="/dashboard" className="hover:text-gray-300">DashBoard</Link>
+           )}
+           {!token && (
+              <Link to="/register" className="hover:text-gray-300">SignUp</Link>
+           )}
+           
+           {token && (
+              <button onClick={logOutHandler} className="hover:text-red-300" >
+                Log Out
+              </button>
+           )}
         </nav>
 
       </div>
